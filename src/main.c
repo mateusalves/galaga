3eeccfb8b3e2a53eb32a7500fdeeb4e265bdb1f8
Mateus Alves <mateus.alves.unb@gmail.com>
2021-05-28 11:05:48 -0300

    Cleaning up code. Also, creating timeout on server response.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include <netinet/in.h>
 #include <stdint.h>
 
-#define PORT    1981 //8080
+#define PORT    1981
 #define MAXLINE 1024
 
 int hex_print(unsigned char *in, size_t len)
@@ -34,10 +34,12 @@ int analyzeData(uint8_t xorkey, char buffer[], uint16_t frame, uint16_t input[],
         uint16_t SEQ, uint16_t numberOfObjetcs, unsigned char decriptedString[])
 {
     printf("\nCurrent Frame 0x%02X\n", frame >> 1);
-    printf("\nXorkey: 0x%02X - Buffer[0]: 0x%02x\n", xorkey, buffer[0]);
+    /*printf("\nXorkey: 0x%02X - Buffer[0]: 0x%02x\n", xorkey, buffer[0]);*/
     printf("Input 0x%02X\n", input[0] | input[1]);
     printf("SEQ 0x%02X\n", SEQ);
     printf("Number Of Objects 0x%02X\n\n", numberOfObjetcs);
+
+    int isMyShipIntact = 1;
 
     if (numberOfObjetcs > 0x00)
     {
@@ -45,31 +47,30 @@ int analyzeData(uint8_t xorkey, char buffer[], uint16_t frame, uint16_t input[],
         {
             printf("\tObject: 0x%02X\n", i);
             printf("\t\tType: 0x%02X\n", decriptedString[i * 3]);
+
+            if (decriptedString[i * 3] == 0x00)
+                isMyShipIntact--;
+
             printf("\t\tH position: 0x%02X\n", decriptedString[(i * 3) + 1]);
             printf("\t\tV position: 0x%02X\n", decriptedString[(i * 3) + 2]);
         }
         printf("\n\n");
-        return 0;
+        return isMyShipIntact;
     }
     else
-        return 1;
+        return -1;
 }
 
 char* decryptString(char inpString[], const int len, uint8_t xorKey)
 {
-    printf("decryptString: ");
+    printf("Decrypted String: ");
     for (int i = 0; i < len; i++)
-    {
         inpString[i] = inpString[i] ^ xorKey;
-        /*if(i % 10 == 0)*/
-        /*    printf("\n");*/
-        /*printf("0x%02X ",inpString[i]);*/
-    }
-    /*printf("\n");*/
-    hex_print((unsigned char *)inpString, len);
 
+    hex_print((unsigned char *)inpString, len);
     return inpString;
 }
+
 
 int main() {
     int sockfd;
@@ -77,111 +78,82 @@ int main() {
     uint8_t inputPackage[2] = {0x00, 0x00};
     struct sockaddr_in     servaddr;
 
+    struct timeval timeout;
+    timeout.tv_usec = 500000;
 
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed\n");
         exit(EXIT_FAILURE);
     }
 
+    if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
+        perror("setsockopt failed\n");
+        exit(EXIT_FAILURE);
+    }
     memset(&servaddr, 0, sizeof(servaddr));
 
     // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = inet_addr("18.219.219.134"); //INADDR_ANY;
+    servaddr.sin_addr.s_addr = inet_addr("18.219.219.134");
 
     unsigned int n, len;
-
-    sendto(sockfd, &inputPackage, sizeof(inputPackage),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Message sent to server...\n\n");
-
-    n = recvfrom(sockfd, (unsigned char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-
-
-    printf("Received message with size: %d\n", n);
-    printf("Server message: ");
-    hex_print((unsigned char *)buffer, n);
-
-    uint8_t xorkey = buffer[0];
-
+    uint8_t xorkey;
     unsigned char *decriptedString ;
-    decriptedString = (unsigned char *) decryptString(buffer, n, xorkey);
 
-    uint16_t frame  = decriptedString[0] & 0xFE;
-    uint16_t input[2] = {decriptedString[0] & 0x01, (decriptedString[1] & 0x80) >> 6};
-    uint16_t SEQ  = decriptedString[1] & 0x7F;
-    uint16_t numberOfObjetcs  = decriptedString[2];
+    uint16_t frame = 0x00;
+    uint16_t input[2] = {0x00, 0x00};
+    uint16_t SEQ;
+    uint16_t numberOfObjetcs;
 
-    int result = analyzeData(xorkey, buffer, frame, input, SEQ, numberOfObjetcs, decriptedString);
-    printf("\n\nResult: %d\n\n", result);
+    int result;
 
-    inputPackage[0] = ((frame + 0x01) << 1) + 0x01;
-    inputPackage[1] = SEQ + 0x80;
+    while(frame != 0xFE)
+    {
+        sendto(sockfd, &inputPackage, sizeof(inputPackage),
+            MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+                sizeof(servaddr));
+        printf("Message sent to server...\n\n");
 
-    printf("%02x%02x\n", inputPackage[0], inputPackage[1]);
+        n = recvfrom(sockfd, (unsigned char *)buffer, MAXLINE,
+                    MSG_WAITALL, (struct sockaddr *) &servaddr,
+                    &len);
 
-    memset(&buffer, 0, n);
+        printf("\n\nn : %d\n\n", n);
 
-    sendto(sockfd, &inputPackage, sizeof(inputPackage),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Message sent to server...\n\n");
+        if (n == -1)
+            continue;
 
-    n = recvfrom(sockfd, (unsigned char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
+        printf("Received message with size: %d\n", n);
+        printf("Server message: ");
+        hex_print((unsigned char *)buffer, n);
 
-    printf("Received message with size: %d\n", n);
-    printf("Server message: ");
-    hex_print((unsigned char *)buffer, n);
+        xorkey = buffer[0] ^ inputPackage[0];
 
-    xorkey = buffer[0] ^ (inputPackage[0]);
-    decriptedString = (unsigned char *) decryptString(buffer, n, xorkey);
+        decriptedString = (unsigned char *) decryptString(buffer, n, xorkey);
 
-    frame  = decriptedString[0] & 0xFE;
-    input[0] = decriptedString[0] & 0x01;
-    input[1] = (decriptedString[1] & 0x80) >> 6;
-    SEQ  = decriptedString[1] & 0x7F;
-    numberOfObjetcs  = decriptedString[2];
+        frame  = decriptedString[0] & 0xFE;
+        input[0] = decriptedString[0] & 0x01;
+        input[1] = (decriptedString[1] & 0x80) >> 6;
+        SEQ = decriptedString[1] & 0x7F;
+        numberOfObjetcs  = decriptedString[2];
 
-    result = analyzeData(xorkey, buffer, frame, input, SEQ, numberOfObjetcs, decriptedString);
-    printf("\n\nResult: %d\n\n", result);
+        result = analyzeData(xorkey, buffer, frame, input, SEQ, numberOfObjetcs, decriptedString);
+        printf("\n\n\tResult: %d\n\n", result);
 
-    inputPackage[0] = (((frame >> 1) + 0x01) << 1) + 0x01;
-    inputPackage[1] = SEQ + 0x80;
+        if(result != 0)
+        {
+            printf("\n\n\t********** GAME OVER **********\n\n");
+            break;
+        }
 
-    memset(&buffer, 0, n);
+        inputPackage[0] = (((frame >> 1) + 0x01) << 1) + 0x01;
+        inputPackage[1] = SEQ + 0x80;
+        memset(&buffer, 0, n);
 
-    sendto(sockfd, &inputPackage, sizeof(inputPackage),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Message sent to server...\n\n");
-
-    n = recvfrom(sockfd, (unsigned char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-
-    printf("Received message with size: %d\n", n);
-    printf("Server message: ");
-    hex_print((unsigned char *)buffer, n);
-
-    xorkey = buffer[0] ^ (inputPackage[0]);
-    decriptedString = (unsigned char *) decryptString(buffer, n, xorkey);
-
-    frame  = decriptedString[0] & 0xFE;
-    input[0] = decriptedString[0] & 0x01;
-    input[1] = (decriptedString[1] & 0x80) >> 6;
-    SEQ  = decriptedString[1] & 0x7F;
-    numberOfObjetcs  = decriptedString[2];
-
-    result = analyzeData(xorkey, buffer, frame, input, SEQ, numberOfObjetcs, decriptedString);
-    printf("\n\nResult: %d\n\n", result);
-
+        printf("%02x%02x\n", inputPackage[0], inputPackage[1]);
+    }
     close(sockfd);
 
     return 0;
