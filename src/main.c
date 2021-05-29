4bf923a83ef7d5ce1df0516a228e45277955a03a
Mateus Alves <mateus.alves.unb@gmail.com>
2021-05-29 00:48:08 -0300

    Record of 45 frames

--- src/main.c
+++ src/main.c
@@ -30,38 +30,213 @@ int hex_print(unsigned char *in, size_t len)
     return 0;
 }
 
-int analyzeData(uint8_t xorkey, char buffer[], unsigned int n,  uint16_t frame, uint16_t input[],
+typedef struct{
+    int status;
+    uint8_t type;
+    uint8_t x_coordinate;
+    uint8_t y_coordinate;
+    uint8_t nextMoveA;
+    uint8_t nextMoveB;
+}ShipCoordinates;
+
+int nextMove(int numberOfObjetcs, int *types, int *h_positions, int *v_positions, ShipCoordinates *ship)
+{
+    for (int i = 0; i < numberOfObjetcs; i++)
+    {
+        if (types[i] == 0x03)
+        {
+            if (h_positions[i] == ship->x_coordinate)
+            {
+                if (v_positions[i] <= (ship->y_coordinate + 0x02))
+                {
+                    if (ship->x_coordinate == 0x08)
+                    {
+                        //Move right
+                        ship->nextMoveA = 0x00;
+                        ship->nextMoveB = 0x80;
+                    }
+                    else
+                    {
+                        //Move left
+                        ship->nextMoveA = 0x01;
+                        ship->nextMoveB = 0x00;
+                    }
+                    break;
+                }
+            }
+            if (h_positions[i] == (ship->x_coordinate + 0x01))
+            {
+                if (v_positions[i] <= (ship->y_coordinate + 0x02))
+                {
+                    if (ship->x_coordinate == 0x07)
+                    {
+                        //Dont Move
+                        ship->nextMoveA = 0x00;
+                        ship->nextMoveB = 0x00;
+                    }
+                    else
+                    {
+                        //Move left
+                        ship->nextMoveA = 0x01;
+                        ship->nextMoveB = 0x00;
+                    }
+                    break;
+                }
+            }
+            if (h_positions[i] == (ship->x_coordinate - 0x01))
+            {
+                if (v_positions[i] <= (ship->y_coordinate + 0x02))
+                {
+                    if (ship->x_coordinate == 0x08)
+                    {
+                        //Dont Move
+                        ship->nextMoveA = 0x00;
+                        ship->nextMoveB = 0x00;
+                    }
+                    else
+                    {
+                        //Move left
+                        ship->nextMoveA = 0x01;
+                        ship->nextMoveB = 0x00;
+                    }
+                    break;
+                }
+            }
+        }
+        if (types[i] == 0x02)
+        {
+            if (h_positions[i] == ship->x_coordinate)
+            {
+                //Shoot
+                ship->nextMoveA = 0x01;
+                ship->nextMoveB = 0x80;
+            }
+            if (h_positions[i] == (ship->x_coordinate + 0x01) ||
+                    h_positions[i] == (ship->x_coordinate - 0x01))
+            {
+                //Shoot
+                ship->nextMoveA = 0x01;
+                ship->nextMoveB = 0x80;
+            }
+            else if (h_positions[i] > ship->x_coordinate)
+            {
+                //Move left
+                ship->nextMoveA = 0x01;
+                ship->nextMoveB = 0x00;
+            }
+            else
+            {
+                //Move right
+                ship->nextMoveA = 0x00;
+                ship->nextMoveB = 0x80;
+            }
+        }
+    }
+    /*printf("Next MOVE: 0x%02X\n\n", ship->nextMoveA | ship->nextMoveB);*/
+    return 0;
+}
+
+ShipCoordinates analyzeData(uint8_t xorkey, char buffer[], unsigned int n,  uint16_t frame, uint16_t input[],
         uint16_t SEQ, uint16_t numberOfObjetcs, unsigned char decriptedString[])
 {
+    double objectsData = n - 3.0;
+    /*int isMyShipIntact = 0;*/
+    ShipCoordinates ship;
+    ship.status = 0;
+    int *types = malloc(sizeof(int)*numberOfObjetcs);
+    int *h_positions = malloc(sizeof(int)*numberOfObjetcs);
+    int *v_positions = malloc(sizeof(int)*numberOfObjetcs);
+
+
     printf("\nCurrent Frame %d\n", frame >> 1);
     printf("Input %d\n", input[0] | input[1]);
     /*printf("SEQ 0x%02X\n", SEQ);*/
     printf("Number Of Objects %d\n\n", numberOfObjetcs);
 
-    double objectsData = n - 3.0;
-    int isMyShipIntact = 0;
-
     if (numberOfObjetcs > 0x00)
     {
         if(objectsData / numberOfObjetcs < 1.0)
-             return -1;
+        {
+            ship.status = -1;
+            free(types);
+            free(h_positions);
+            free(v_positions);
+            return ship;
+        }
 
         for (int i = 1 ; i <= numberOfObjetcs ; i++)
         {
             printf("\tObject: %d\n", i);
             printf("\t\tType: %d\n", decriptedString[i * 3]);
-
-            if (decriptedString[i * 3] == 0x00)
-                isMyShipIntact = 1;
-
             printf("\t\tH position: %d\n", decriptedString[(i * 3) + 1]);
             printf("\t\tV position: %d\n", decriptedString[(i * 3) + 2]);
+
+            if (decriptedString[i * 3] == 0x00)
+            {
+                ship.status = 1;
+                ship.type = 0x00;
+                ship.x_coordinate = decriptedString[(i * 3) + 1];
+                ship.y_coordinate = decriptedString[(i * 3) + 2];
+            }
+            *(types + i - 1) = decriptedString[i * 3];
+            *(h_positions + i - 1) = decriptedString[(i * 3) + 1];
+            *(v_positions + i - 1) = decriptedString[(i * 3) + 2];
         }
         printf("\n\n");
-        return isMyShipIntact;
+
+        if (ship.status != 0)
+            nextMove(numberOfObjetcs, types, h_positions, v_positions, &ship);
     }
     else
-        return -1;
+        ship.status = -1;
+
+    free(types);
+    free(h_positions);
+    free(v_positions);
+    return ship;
+}
+
+void readInputFromKeyboard(ShipCoordinates *ship)
+{
+    char choice;
+    printf("Enter your move: ");
+    scanf(" %c",&choice);
+    switch (choice) {
+      case 'w':
+      case 'W':
+        //shoot
+        ship->nextMoveA = 0x01;
+        ship->nextMoveB = 0x80;
+        break;
+      case 'a':
+      case 'A':
+         // Move left
+        ship->nextMoveA = 0x01;
+        ship->nextMoveB = 0x00;
+        break;
+      case 's':
+      case 'S':
+         // Dont Move
+        ship->nextMoveA = 0x00;
+        ship->nextMoveB = 0x00;
+        break;
+      case 'd':
+      case 'D':
+        // Move right
+        ship->nextMoveA = 0x00;
+        ship->nextMoveB = 0x80;
+        break;
+      case 'q':
+      case 'Q':
+        printf("\nQuitting...\n");
+        ship->nextMoveA = 0xFF;
+        ship->nextMoveB = 0xFF;
+        break;
+      default:
+        printf("\nWrong input...\n");
+        ship->nextMoveA = 0x00;
+        ship->nextMoveB = 0x00;
+    }
 }
 
 char* decryptString(char inpString[], const int len, uint8_t xorKey)
@@ -73,7 +248,6 @@ char* decryptString(char inpString[], const int len, uint8_t xorKey)
     hex_print((unsigned char *)inpString, len);
     return inpString;
 }
-
 
 int main() {
     int sockfd;
@@ -89,7 +263,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
     {
         perror("setsockopt failed\n");
         exit(EXIT_FAILURE);
@@ -104,31 +278,37 @@ int main() {
     unsigned int n, len;
     uint8_t xorkey;
     unsigned char *decriptedString ;
+    ShipCoordinates myShip;
+    myShip.nextMoveA = 0x00;
+    myShip.nextMoveB = 0x00;
 
     uint16_t frame = 0x00;
-    uint16_t input[2] = {0x00, 0x00};
+    uint16_t input[2] = {myShip.nextMoveA, myShip.nextMoveB};
     uint16_t SEQ;
     uint16_t numberOfObjetcs;
 
-    int result;
+    printf("\n\t\t\t\t\tBeating Galaga Game\n");
+    printf("\n\tControls: s - Don't move/Start; a - Move left; d - Move right; w - Shoot; q - Quit\n");
+    printf("\n\t\t\t\t\t\tHave fun =]\n\n");
 
+    /*for(int i = 0; i  < 4 ; i++)*/
     while(frame != 0xFE)
     {
         sendto(sockfd, &inputPackage, sizeof(inputPackage),
             MSG_CONFIRM, (const struct sockaddr *) &servaddr,
                 sizeof(servaddr));
-        printf("Message sent to server...\n\n");
+        /*printf("Message sent to server...\n\n");*/
 
         n = recvfrom(sockfd, (unsigned char *)buffer, MAXLINE,
                     MSG_WAITALL, (struct sockaddr *) &servaddr,
                     &len);
 
-        printf("\n\nn : %d\n\n", n);
+        printf("\nn : %d\n", n);
 
         if (n == -1)
             continue;
 
-        printf("Received message with size: %d\n", n);
+        /*printf("Received message with size: %d\n", n);*/
         printf("Server message: ");
         hex_print((unsigned char *)buffer, n);
 
@@ -142,22 +322,30 @@ int main() {
         SEQ = decriptedString[1] & 0x7F;
         numberOfObjetcs  = decriptedString[2];
 
-        result = analyzeData(xorkey, buffer, n, frame, input, SEQ, numberOfObjetcs, decriptedString);
-        printf("\n\n\tResult: %d\n\n", result);
+        myShip = analyzeData(xorkey, buffer, n, frame, input, SEQ, numberOfObjetcs, decriptedString);
 
-        if(result == 0)
+        if(myShip.status == 0)
         {
             printf("\n\n\t********** GAME OVER **********\n\n");
             break;
         }
-        else if(result == -1)
+        else if(myShip.status == -1)
             continue;
 
-        inputPackage[0] = (((frame >> 1) + 0x01) << 1) + 0x01;
-        inputPackage[1] = SEQ + 0x80;
+        /*readInputFromKeyboard(&myShip);*/
+
+        /*if ((myShip.nextMoveA & myShip.nextMoveB) == 0xFF)*/
+        /*{*/
+        /*    printf("\n\n\t********** GAME OVER **********\n\n");*/
+        /*    break;*/
+        /*}*/
+
+        printf("Next Move: 0x%02X", myShip.nextMoveA | myShip.nextMoveB);
+        inputPackage[0] = (((frame >> 1) + 0x01) << 1) + myShip.nextMoveA;
+        inputPackage[1] = SEQ + myShip.nextMoveB;
         memset(&buffer, 0, n);
 
-        printf("%02x%02x\n", inputPackage[0], inputPackage[1]);
+        /*printf("%02x%02x\n", inputPackage[0], inputPackage[1]);*/
     }
     close(sockfd);
 
